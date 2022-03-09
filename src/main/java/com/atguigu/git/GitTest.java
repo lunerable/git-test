192b8dbdc2c3c8c625d8b4965b63f8b4102c2b57
luner <3198738484@qq.com>
2022-03-09 09:10:29 +0000

    update src/main/java/com/atguigu/git/GitTest.java.

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -10,5 +10,6 @@ public class GitTest {
         System.out.println("push test!");
         System.out.println("pull test!");
         System.out.println("git test!");
+        System.out.println("git test2!");
     }
 }
